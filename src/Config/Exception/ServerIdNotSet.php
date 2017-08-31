69153f3ba3fb504402da150fc6070d68c03c1342
Fwolf <fwolf.aide+git@gmail.com>
2017-08-31 16:55:30 +0800

    New exceptions used in CheckServerIdTrait

---
+++ src/Config/Exception/ServerIdNotSet.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fwolf\Config\Exception;
+
+/**
+ * Exception for not set server id when checked
+ *
+ * @copyright   Copyright 2015-2017 Fwolf
+ * @license     https://opensource.org/licenses/MIT MIT
+ */
+class ServerIdNotSet extends \Exception
+{
+}
---
+++ src/Config/Exception/ServerIdProhibited.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fwolf\Config\Exception;
+
+/**
+ * Exception for program not allowed run on this server
+ *
+ * @copyright   Copyright 2015-2017 Fwolf
+ * @license     https://opensource.org/licenses/MIT MIT
+ */
+class ServerIdProhibited extends \Exception
+{
+}
